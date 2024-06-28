de22a9dd498d964cffa12b1e7a08bb8bcd743f86
Serhii Balaba <balabaserhii@gmail.com>
2024-06-28 22:45:29 +0200

    ы

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { fetchContacts } from "../../redux/contacts/operations.js";
 import {
   selectError,
