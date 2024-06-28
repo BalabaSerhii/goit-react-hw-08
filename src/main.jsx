164142adf1e608e52725dd3fea840d37ec06423b
Serhii Balaba <balabaserhii@gmail.com>
2024-06-28 12:24:50 +0200

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
-import App from './components/App/App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./redux/store.js";
+import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store.js";
+import App from "./components/App/App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
